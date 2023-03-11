98cd5caffa25570d580b81d9af852b80f768717b
pl4cydo <psl2@discente.ifpe.edu.br>
2023-03-11 14:26:54 -0300

    criação do modelo visal

---
+++ src/componnet/placar.jsx
@@ -0,0 +1,17 @@
+function placar() {
+    return (
+        <div className="placar">
+            <div className="numeros">
+                <h1>0</h1>
+                <h2>x</h2>
+                <h1>0</h1>
+            </div>
+            <div className="times">
+                <h1>HOME</h1>
+                <h1>GUEST</h1> 
+            </div>
+        </div>
+    )
+}
+
+export default placar;
---
+++ src/componnet/pontos.jsx
@@ -0,0 +1,10 @@
+function pontos() {
+    return (
+        <div className="pontos">
+            <div className="button"><p>:D</p></div>
+            <div className="button"><p>:(</p></div>
+        </div>
+    );
+};
+
+export default pontos;
